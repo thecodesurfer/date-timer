f4eb7bc884b1d20c8513f5dacef4e9dc835ab610
thecodesurfer <rubenfalkenburg@gmail.com>
2021-02-14 00:09:04 +0100

    Added timer input

--- index.js
+++ index.js
@@ -1,63 +1,122 @@
-let timerID;
+let _timerID;
+let _targetDate = null;
 
-const divDate       = document.getElementById("date");
+const searchParams = new URLSearchParams(window.location.search);
 
-const counterDays   = document.getElementById("counter-days");
-const counterHours  = document.getElementById("counter-hours");
-const counterMins   = document.getElementById("counter-mins");
-const counterSecs   = document.getElementById("counter-secs");
+const btnStartTimer = document.getElementById('btnStartTimer');
+const btnClearTimer = document.getElementById('btnClearTimer');
 
-const btnClear      = document.getElementById("counter-clear");
+const divWrapperCounterForm     = document.getElementById('wrapper-counter-form');
+const divWrapperCounter         = document.getElementById('wrapper-counter');
+const divCounterTargetDate      = document.getElementById('counter-targetdate');
+const divCounterDays            = document.getElementById('counter-days');
+const divCounterHours           = document.getElementById('counter-hours');
+const divCounterMinutes         = document.getElementById('counter-minutes');
+const divCounterSeconds         = document.getElementById('counter-seconds');
 
-function initializeCounter()
-{
-    const url = new URL(location.href);
+btnStartTimer.addEventListener('click', function() {
 
-    const targetDate =  url.searchParams.get('targetdate');
+    const day       = document.getElementById('input-day').value;
+    const month     = document.getElementById('input-month').value;
+    const year      = document.getElementById('input-year').value;
 
-    console.log(targetDate)
+    const targetDate = new Date(year, month - 1, day);
 
-    if(targetDate)
-    {
-        const date = new Date(targetDate);
-
-        divDate.innerHTML = moment(date).format('MMMM Do YYYY h:mm:ss a');
-
-        timerID = setInterval(function() {
-
-            const now = moment();
-            const tmpDate = moment(date);
+    if(isValidDate(targetDate)) {
+        _targetDate = targetDate;
+        _timerID = startTimer(targetDate)
         
-            const diffDays  = tmpDate.diff(now, 'days');
-            const diffHours = tmpDate.subtract(diffDays, 'days').diff(now, 'hours');
-            const diffMins  = tmpDate.subtract(diffHours, 'hours').diff(now, 'minutes');
-            const diffSecs  = tmpDate.subtract(diffMins, 'minutes').diff(now, 'seconds');
-        
-            counterDays.innerHTML   = diffDays.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
-            counterHours.innerHTML  = diffHours.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
-            counterMins.innerHTML   = diffMins.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
-            counterSecs.innerHTML   = diffSecs.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
-        
-        }, 1000);
+        divWrapperCounterForm.classList.add('hide');
+        divWrapperCounter.classList.remove('hide');
+        divCounterTargetDate.innerHTML = moment(targetDate).format('MMMM Do YYYY');
+
+        searchParams.set('targetdate', moment(targetDate).format('YYYY-MM-DD'))
+
+        if(history.pushState) {
+            const newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + "?" +searchParams.toString();
+            window.history.pushState({ path: newurl}, '', newurl);
+        }
     }
-    else {
-        clearInterval(timerID);
+})
 
-        // const input = prompt('Target date (YYYY-MM-DD hh:mm:ss):');
+btnClearTimer.addEventListener('click', function() {
+    clearTimer(_timerID);
 
-        // console.log(input);
+    divWrapperCounter.classList.add('hide');
+    divWrapperCounterForm.classList.remove('hide');
 
-        // if(input) {
-        //     // window.localStorage.setItem('targetDate', input);
-        //     url.searchParams.set('targetdate', new Date(input).toISOString())
-        // }
+    searchParams.delete('targetdate');
 
-        // initializeCounter();
+    if(history.pushState) {
+        const newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + searchParams.toString();
+        window.history.pushState({ path: newurl}, '', newurl);
+    }
+})
+
+function startTimer(date) {
+    return setInterval(() => {
+        
+        const now = moment();
+        const targetDate = moment(date);
+
+        if(moment(now).isAfter(targetDate))
+        {
+            clearTimer(_timerID);
+        }
+        else
+        {
+            const diffDays      = targetDate.diff(now, 'days');
+            const diffHours     = targetDate.subtract(diffDays, 'days').diff(now, 'hours');
+            const diffMinutes   = targetDate.subtract(diffHours, 'hours').diff(now, 'minutes');
+            const diffSeconds   = targetDate.subtract(diffMinutes, 'minutes').diff(now, 'seconds');
+
+            divCounterDays.innerHTML        = diffDays.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
+            divCounterHours.innerHTML       = diffHours.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
+            divCounterMinutes.innerHTML     = diffMinutes.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
+            divCounterSeconds.innerHTML     = diffSeconds.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
+        }
+
+    }, 1000);
+}
+
+function clearTimer(timerID) {
+    _targetDate = null;
+    clearInterval(timerID);
+
+    divCounterDays.innerHTML        = "000";
+    divCounterHours.innerHTML       = "00";
+    divCounterMinutes.innerHTML     = "00";
+    divCounterSeconds.innerHTML     = "00";
+}
+
+function initializeTimer() {
+    const targetdateParam = searchParams.get('targetdate');
+
+    if (targetdateParam)
+    {
+        const targetDate = new Date(targetdateParam);
+        
+        if(isValidDate(targetDate)) {
+            divWrapperCounterForm.classList.add('hide');
+            divWrapperCounter.classList.remove('hide');
+
+            targetDate.setHours(00, 00, 00);
+
+            divCounterTargetDate.innerHTML = moment(targetDate).format('MMMM Do YYYY');
+
+            _timerID = startTimer(targetDate)
+        }
     }
 }
 
-initializeCounter();
+function isValidDate(date) {
+    if(Object.prototype.toString.call(date) === '[object Date]'){
+        if (!isNaN(date.getTime())) {
+            return true;
+        }
+    }
 
-btnClear.addEventListener('click', function() {
-    initializeCounter();
-})
+    return false;
+}
+
+initializeTimer();
